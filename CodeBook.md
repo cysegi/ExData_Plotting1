a7f2595067db85be681a60deb25a2825548aa383
cysegi <cysegi@gmail.com>
2015-06-07 14:53:58 -0700

    Exploratory Data Analysis First Assignment

---
+++ CodeBook.md
@@ -0,0 +1,16 @@
+# Exploratory Data Analysis Assignment 1 CodeBook
+===================================================
+
+This set of assignment contents 5 R programming codes in order to delivered the requirements of assignment 1. The separations of code deliver the following description:
+
+* EDA-assignment1.R
+	EDA-assignment1.R first test the source availability. It will download the source if data source are not exist.
+	Next define 9 variables in the dataset as listed in README.md. Once data subset is ready, and a file name "household_power_consumption" create
+* plot1.R
+	Frequency to Global Active Power
+* plot2.R
+	Global Active Power to Days
+* plot3.R
+	Energy Sub Metering to Days
+* plot4.R
+	Combination of Graph Analysis
---
+++ EDA-assignment1.R
@@ -0,0 +1,33 @@
+#Data Source Check and retrieving
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+        temp <- tempfile()
+        download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+        file <- unzip(temp)
+        unlink(temp)
+}
+#Reading Source
+power <- read.table(file, header=T, sep=";")
+#Define variables 
+#
+#The following descriptions of the 9 variables in the dataset are taken from the UCI web site:
+#
+#Date: Date in format dd/mm/yyyy
+#Time: time in format hh:mm:ss
+#Global_active_power: household global minute-averaged active power (in kilowatt)
+#Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+#Voltage: minute-averaged voltage (in volt)
+#Global_intensity: household global minute-averaged current intensity (in ampere)
+#Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+#Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+#Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+#Data Subset completed.
+cat("Data Sub Set is now ready")
